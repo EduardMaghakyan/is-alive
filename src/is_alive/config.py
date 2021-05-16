3badd3fc0a9166ce8d47efadfa668b2170249f45
Eduard Maghakyan <edi.maghakyan@gmail.com>
2021-05-17 00:34:25 +0200

    Remove duplicate config

--- src/is_alive/config.py
+++ src/is_alive/config.py
@@ -1,14 +1,12 @@
 from environs import Env
 
 env = Env()
-APPLICATION_ENV = env("APPLICATION_ENV", None)
+APPLICATION_ENV = env("APPLICATION_ENV", "dev")
 env_path = f".env.{APPLICATION_ENV}" if APPLICATION_ENV else None
 env.read_env(env_path)
 
-application_env = env("APPLICATION_ENV", "dev")
-
 CONFIG: dict = {
-    "app": {"env": application_env},
+    "app": {"env": APPLICATION_ENV},
     "ports": {
         "event_publisher": {
             "type": env("EVENT_PUBLISHER_TYPE", "memory"),
